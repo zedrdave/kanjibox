4ee01079215f899f55499e1addcad18d3e275509
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2014-04-28 18:02:46 +0900

    Adjusted .htaccess file (from production server). Updated mark_msg_read to use PDO. Made initial changes (PDO) to Kana class.

--- ajax_scripts/mark_msg_read.php
+++ ajax_scripts/mark_msg_read.php
@@ -1,11 +1,16 @@
 <?php
-if(! @$_SESSION['user'])
-{
-	log_error('You need to be logged-in to access this function.', false, true);
+
+if (empty($_SESSION['user'])) {
+    log_error('You need to be logged-in to access this function.', false, true);
 }
 
-$query = 'UPDATE `messages` SET msg_read = 1 WHERE user_id_to = ' . (int) $_SESSION['user']->getID() . " AND message_id = " . (int) @$params['id'];
-$res = mysql_query_debug($query) or log_db_error($query, '', false, true);
+$query = 'UPDATE `messages` SET msg_read = 1 WHERE user_id_to = ? AND message_id = ?';
+try {
+    $stmt = DB::getConnection()->prepare($query);
+    $stmt->execute([$_SESSION['user']->getID(), $params['id']]);
+    $stmt = null;
 
-echo 'Marked as read!'
-?>
+    echo 'Marked as read!';
+} catch (PDOException $e) {
+    log_db_error($query, $e->getMessage(), false, true);
+}
--- classes/Kana.php
+++ classes/Kana.php
@@ -49,7 +49,7 @@ class Kana extends Question {
         } elseif (!$_SESSION['user']) {
             $picks = $this->get_random_kanas($how_many * 4);
         } else {
-            $picks = $this->get_random_weighted_kanas($user_id, $how_many * 4);
+            $picks = $this->getRandomWeightedKanas($user_id, $how_many * 4);
         }
 
         for ($i = 0; $i < count($picks) - 3; $i+=4) {
@@ -60,12 +60,13 @@ class Kana extends Question {
                 $choice[$j] = $picks[$i + $j];
                 $exclude[] = $choice[$j]->roma;
             }
-            if ($j < 4)
+            if ($j < 4) {
                 if ($_SESSION['user']) {
                     $filler = $this->get_random_kanas((4 - $j), $choice[0]->type, $exclude);
                 } else {
-                    $filler = $this->get_random_weighted_kanas($user_id, (4 - $j), $choice[0]->type, $exclude);
+                    $filler = $this->getRandomWeightedKanas($user_id, (4 - $j), $choice[0]->type, $exclude);
                 }
+            }
 
             while ($j++ < 4) {
                 $choice[$j] = $filler[3 - $j];
@@ -90,27 +91,33 @@ class Kana extends Question {
         return mysql_fetch_object($res);
     }
 
-    function get_random_weighted_kanas($user_id, $how_many = 1, $type = NULL, $exclude = NULL) {
-        $where_type = ($type ? 'k.type = \'' . mysql_real_escape_string($type) . "'" : '1');
+    function getRandomWeightedKanas($user_id, $how_many = 1, $type = NULL, $exclude = NULL) {
+        $inputValues = [$user_id];
+        $where_type = '1';
+        if (isset($type)) {
+            $where_type = 'k.type = ?';
+            $inputValues[] = $type;
+        }
+
         $where_exclude = ($exclude ? 'k.roma NOT IN (' . implode(', ', $exclude) . ')' : '1');
-        $query = "SELECT * FROM (SELECT  k.`id`, k.`kana`,  UPPER(k.`roma`) AS `roma`, k.`type`, IF(l.curve IS NULL, 1000, curve)+1000*rand() as xcurve from kanas k left join " . $this->table_learning . " l on l.user_id = '" . (int) $user_id . "' AND k.id = l." . $this->table_learning_index . " WHERE $where_type AND $where_exclude ORDER BY xcurve DESC LIMIT " . $how_many . ') as temp ORDER BY type';
+        $query = 'SELECT * FROM (SELECT  k.`id`, k.`kana`,  UPPER(k.`roma`) AS `roma`, k.`type`, IF(l.curve IS NULL, 1000, curve)+1000*rand() as xcurve from kanas k left join ' . $this->table_learning . ' l on l.user_id = ? AND k.id = l.' . $this->table_learning_index . " WHERE $where_type AND $where_exclude ORDER BY xcurve DESC LIMIT " . $how_many . ') as temp ORDER BY type';
 
-        // echo $query;
-        $res = mysql_query_debug($query) or log_db_error($query);
-        if (mysql_num_rows($res) < $how_many) {
-            log_error("Can't get enough randomized kanas: " . $query, false, true);
+        try {
+            $stmt = DB::getConnection()->prepare($query);
+            $stmt->execute($inputValues);
+
+            if ($stmt->rowCount() < $how_many) {
+                log_error('Can\'t get enough randomized kanas: ' . $query, false, true);
+            }
+
+            if ($how_many == 1) {
+                return $stmt->fetch(PDO::FETCH_CLASS);
+            }
+
+            return $stmt->fetchAll(PDO::FETCH_CLASS);
+        } catch (PDOException $e) {
+            log_db_error($query, $e->getMessage(), false, true);
         }
-
-        if ($how_many == 1) {
-            return $row;
-        }
-
-        $kanas = array();
-        while ($row = mysql_fetch_object($res)) {
-            $kanas[] = $row;
-        }
-
-        return $kanas;
     }
 
     function get_random_kanas($how_many = 1, $type = NULL, $exclude = NULL) {
